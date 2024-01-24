f49dbc48f342dcf2702f4b6011babb0c6465b6bf
Gebhartj <34003931+Gebhartj@users.noreply.github.com>
2024-01-24 09:31:36 +0100

    Revert "repair inscorect directive .offset to .origin"

--- adafruit_pioasm.py
+++ adafruit_pioasm.py
@@ -63,7 +63,7 @@ class Program:  # pylint: disable=too-few-public-methods
                 if program_name:
                     raise RuntimeError("Multiple programs not supported")
                 program_name = line.split()[1]
-            elif line.startswith(".origin"):
+            elif line.startswith(".offset"):
                 offset = int(line.split()[1], 0)
             elif line.startswith(".wrap_target"):
                 wrap_target = len(instructions)
